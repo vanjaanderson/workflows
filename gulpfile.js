4f077235976e801ae8e5c024c77cbd10737ac904
VanjaAnderson <vanja.m.anderson@gmail.com>
2017-11-29 23:39:19 +0100

    Installd gulp and gulp util

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+
+gulp.task('log', function() {
+	gutil.log('Workflows are awesome');
+});
+
+/*gulp.task("default", function () {
+  gulp.start("serve");
+});*/
