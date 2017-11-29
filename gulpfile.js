db012cf6ff86cb30f9e151590e09b9092ace38c7
VanjaAnderson <vanja.m.anderson@gmail.com>
2017-11-30 00:21:40 +0100

    Concatenated Javascript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,25 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
-	coffeeSources = ['components/coffee/*.coffee'];
+	concat = require('gulp-concat');
+
+var	coffeeSources = ['components/coffee/*.coffee'];
+var jsSources = [
+	'components/scripts/rclick.js',
+	'components/scripts/pixgrid.js',
+	'components/scripts/tagline.js',
+	'components/scripts/template.js'
+];
 
 gulp.task('coffee', function() {
 	gulp.src('components/coffee/tagline.coffee')
 		.pipe(coffee({bare: true}) // Compiles Javascript
 			.on('error', gutil.log))
 		.pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('js', function() {
+	gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('builds/development/js'))
 });
